e9804116a6f7af2871bf19fa9a5968d134f95fa1
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-17 23:57:56 +0600

    added color using js

--- js-dom/try.js
+++ js-dom/try.js
@@ -2,5 +2,7 @@ const blogs= document.getElementsByTagName('p');
 console.log(blogs);
 
 for(tag of blogs){
-    console.log(tag);
+    // console.log(tag);
+    tag.style.backgroundColor ='blue';
+    tag.style.color ='white';
 }
