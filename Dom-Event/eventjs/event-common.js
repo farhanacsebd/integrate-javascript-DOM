96e8d21129acba91f8e8c6baf4f520c1dfde1dac
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-18 23:17:01 +0600

    set using function

---
+++ Dom-Event/eventjs/event-common.js
@@ -0,0 +1,8 @@
+// set the function 
+
+function handleJustFanctionClick(){
+    const p = document.getElementById('clicking-method');
+    p.innerText ='set by using function';
+}
+
+// direct function 
