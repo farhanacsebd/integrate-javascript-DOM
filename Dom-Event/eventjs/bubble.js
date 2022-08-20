ec68693147217564add813b7a31af06cebe38105
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-20 23:39:47 +0600

    bubble stop immediate propagation

---
+++ Dom-Event/eventjs/bubble.js
@@ -0,0 +1,28 @@
+document.getElementById('list-item').addEventListener('click',function(event){
+    console.log('list1 item');
+    event.stopImmediatePropagation();
+})
+document.getElementById('list-item').addEventListener('click',function(event){
+    console.log('list2 item');
+    
+}),
+document.getElementById('list-item').addEventListener('click',function(event){
+    console.log('list3 item');
+    
+}),
+document.getElementById('list-item').addEventListener('click',function(event){
+    console.log('list4 item');
+    
+}),
+document.getElementById('list-item').addEventListener('click',function(event){
+    console.log('list5 item');
+    
+}),
+// ul
+document.getElementById('list-container').addEventListener('click',function(){
+    console.log('list-container item');
+})
+// section
+document.getElementById('section-container').addEventListener('click',function(){
+    console.log('section-container item');
+})
