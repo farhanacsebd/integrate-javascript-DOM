e5d2e8f9d3dc29c3cafdb4251203edcd98bc3b03
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-23 23:09:20 +0600

    button addevenlistener

---
+++ practicing/javascript-dom/event-button.js
@@ -0,0 +1,11 @@
+function makeRed(){
+
+    document.body.style.backgroundColor = "red";
+
+}
+
+document.getElementById('make-yellow').addEventListener('click',function(){
+
+    document.body.style.backgroundColor ='yellow';
+    
+})
