e536468c54f4213e14447ae1b84f94dbac54ba43
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-27 23:15:09 +0600

    added email passward eventhandler

--- Bank-Tailwind/js/bank.js
+++ Bank-Tailwind/js/bank.js
@@ -1,3 +1,16 @@
 document.getElementById('login-submit').addEventListener('click',function(){
-    console.log('hello');
+    
+
+    // get user email
+
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+
+
+    // get user password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
 })
