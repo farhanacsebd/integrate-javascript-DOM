ee083b1c94cc14ff69e7c56e74acd5e1e53f7543
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-28 16:53:27 +0600

    added dipositeInput evenhandler

---
+++ Bank-Tailwind/js/banking.js
@@ -0,0 +1,6 @@
+document.getElementById('deposite-button').addEventListener('click',function(){
+    
+    const dipositeInput = document.getElementById('deposite-input');
+    const dipositeAmount = dipositeInput.value;
+    console.log(dipositeAmount);
+})
