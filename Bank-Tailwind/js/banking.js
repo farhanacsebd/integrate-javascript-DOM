38240240430415d3c424d5fb9c91ddba957796c3
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-28 22:58:20 +0600

    get the amount diposited

--- Bank-Tailwind/js/banking.js
+++ Bank-Tailwind/js/banking.js
@@ -1,6 +1,15 @@
+// handle diposite button event 
 document.getElementById('deposite-button').addEventListener('click',function(){
     
+    // get the amount diposite 
     const dipositeInput = document.getElementById('deposite-input');
     const dipositeAmount = dipositeInput.value;
-    console.log(dipositeAmount);
+    
+   console.log(dipositeAmount);
+
+  const dipositeTotal = document.getElementById('deposite-total');
+  dipositeTotal.innerText = dipositeAmount;
+
+
+  dipositeInput.value= '';
 })
