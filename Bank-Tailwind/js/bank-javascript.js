dc251254a27644149300fcb7f341aabaae0a2150
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-29 19:51:22 +0600

    Balance Increasing

--- Bank-Tailwind/js/bank-javascript.js
+++ Bank-Tailwind/js/bank-javascript.js
@@ -1,5 +1,7 @@
 document.getElementById('deposite-button').addEventListener('click',function(){
- // diposite input field 
+ 
+// diposite input field 
+
     const dipositeInput = document.getElementById("deposite-input");
     const newDipositeAmount = parseFloat(dipositeInput.value);
 
@@ -9,7 +11,20 @@ const dipositeTotal = document.getElementById('deposite-total');
 const previousDitositeAmount = parseFloat(dipositeTotal.innerText);
 const totalDipositeAmount = previousDitositeAmount + newDipositeAmount;
 dipositeTotal.innerText = totalDipositeAmount;
+
+
+// Balance Increasing
+
+const balanceTotal = document.getElementById('balance-total');
+const balanceTotalAmount = parseFloat(balanceTotal.innerText);
+const totalBalanceAmount = balanceTotalAmount + newDipositeAmount;
+
+balanceTotal.innerText = totalBalanceAmount;
+
+
 // clear value
 
 dipositeInput.value = '';
+
+
 })
