0d452e184d292d3529e2153fa448db09146dbffb
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-29 19:43:19 +0600

    update diposite total

--- Bank-Tailwind/js/bank-javascript.js
+++ Bank-Tailwind/js/bank-javascript.js
@@ -1,8 +1,15 @@
 document.getElementById('deposite-button').addEventListener('click',function(){
  // diposite input field 
     const dipositeInput = document.getElementById("deposite-input");
-    const newDipositeText = dipositeInput.value;
+    const newDipositeAmount = parseFloat(dipositeInput.value);
 
+// diposite value show
 
+const dipositeTotal = document.getElementById('deposite-total');
+const previousDitositeAmount = parseFloat(dipositeTotal.innerText);
+const totalDipositeAmount = previousDitositeAmount + newDipositeAmount;
+dipositeTotal.innerText = totalDipositeAmount;
+// clear value
 
+dipositeInput.value = '';
 })
