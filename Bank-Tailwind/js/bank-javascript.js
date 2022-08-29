afd323e15a1bfd02cba991afa3fc9f9b4aa57e68
farhanacsebd <farhana.cse.bd@gmail.com>
2022-08-29 19:33:20 +0600

    diposite input field

---
+++ Bank-Tailwind/js/bank-javascript.js
@@ -0,0 +1,8 @@
+document.getElementById('deposite-button').addEventListener('click',function(){
+ // diposite input field 
+    const dipositeInput = document.getElementById("deposite-input");
+    const newDipositeText = dipositeInput.value;
+
+
+
+})
