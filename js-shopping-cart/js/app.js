4903c74216dd6bf6571ad89b6902d5b47549a7d1
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-01 23:57:15 +0600

    added event handler

--- js-shopping-cart/js/app.js
+++ js-shopping-cart/js/app.js
@@ -1,1 +1,8 @@
-console.log('hello');
+document.getElementById('iphone-plus-button').addEventListener('click',function(){
+    
+    const firstInput = document.getElementById('first-input');
+    console.log(firstInput.value);
+
+    const iphonAmount = document.getElementById('iphon-amount');
+    // iphonAmount.innerText = firstInput.value;
+})
