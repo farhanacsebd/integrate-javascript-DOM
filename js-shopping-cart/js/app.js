499f49ad3bd048e577da1a7e0f72032026081d0d
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-02 17:18:09 +0600

    handle the nasted function

--- js-shopping-cart/js/app.js
+++ js-shopping-cart/js/app.js
@@ -1,14 +1,25 @@
+function updateproductnumber(increaseNumber){
+    const phoneInput = document.getElementById('phone-number');
+    let phoneNumber =phoneInput.value;
+    if(increaseNumber){
+        phoneNumber = parseInt(phoneInput.value) +1;
+    }
+    else if(phoneNumber>0){
+      phoneNumber = parseInt(phoneInput.value) -1;
+    }
+    phoneInput.value =phoneNumber;
+}
+
+
 document.getElementById('phone-plus').addEventListener('click',function(){
     
-    const phoneInput = document.getElementById('phone-number');
-    const phoneNumber = parseInt(phoneInput.value) +1;
-    phoneInput.value =phoneNumber;
-
+    updateproductnumber(true)
 });     
 document.getElementById('phone-minus').addEventListener('click',function(){
     
-    const phoneInput = document.getElementById('phone-number');
+    /* const phoneInput = document.getElementById('phone-number');
     const phoneNumber = parseInt(phoneInput.value) -1;
-    phoneInput.value =phoneNumber;
+    phoneInput.value =phoneNumber; */
+    updateproductnumber(false)
 
 });     
