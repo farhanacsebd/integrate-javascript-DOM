7f6a5e90ed63d83fa5f99c21ab2b8c143d7610bf
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-02 13:51:05 +0600

    updated all button or input

--- js-shopping-cart/js/app.js
+++ js-shopping-cart/js/app.js
@@ -1,8 +1,8 @@
-document.getElementById('iphone-plus-button').addEventListener('click',function(){
+/* document.getElementById('iphone-plus-button').addEventListener('click',function(){
     
     const firstInput = document.getElementById('first-input');
     console.log(firstInput.value);
 
     const iphonAmount = document.getElementById('iphon-amount');
     console.log(iphonAmount.innerText);
-})
+}) */
