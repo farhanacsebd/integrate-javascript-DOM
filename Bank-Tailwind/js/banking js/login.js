b99a84771d560466c6674cd55bf7d124a6c75254
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-04 00:38:54 +0600

    added handler in login form

--- Bank-Tailwind/js/banking js/login.js
+++ Bank-Tailwind/js/banking js/login.js
@@ -1,3 +1,15 @@
 document.getElementById('login-submit').addEventListener('click',function(){
-    console.log('submit');
+    const inputField = document.getElementById('user-email');const inputValue =inputField.value;
+    
+
+    const inputpass = document.getElementById('user-password');
+    const inputUser = inputpass.value;
+    
+
+    if((inputValue =='farhana') && (inputUser =='1234')){
+        window.location.href='banking.html'
+    }
+    else{
+        alert('rong password')
+    } 
 })
