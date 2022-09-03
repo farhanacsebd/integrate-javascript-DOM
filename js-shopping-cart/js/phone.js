ab1245cd7dec39901f60b074cb3791a7cf36f3cb
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-03 22:56:32 +0600

    increase both in nasted function

--- js-shopping-cart/js/phone.js
+++ js-shopping-cart/js/phone.js
@@ -1,4 +1,4 @@
-function updateproductnumber(product,increaseNumber){
+function updateproductnumber(product,price,increaseNumber){
    const phoneInput = document.getElementById(product + '-number');
    let phoneNumber= phoneInput.value;
    if(increaseNumber){
@@ -8,24 +8,24 @@ function updateproductnumber(product,increaseNumber){
        phoneNumber= parseInt(phoneInput.value) - 1;
    }
    phoneInput.value = phoneNumber;
-   const phoneTotal = document.getElementById('phone-total');
-   phoneTotal.innerText = phoneNumber * 1219;
+   const phoneTotal = document.getElementById(product + '-total');
+   phoneTotal.innerText = phoneNumber * price;
 }
 
 // phone
 document.getElementById('phone-plus').addEventListener('click',function(){
-   updateproductnumber('phone',true)
+   updateproductnumber('phone',1219,true)
 });
 document.getElementById('phone-minus').addEventListener('click',function(){
-   updateproductnumber('phone',false)
+   updateproductnumber('phone',1219,false)
 });
 
 
 // case
 
 document.getElementById('case-plus').addEventListener('click',function(){
-   updateproductnumber('case',true)
+   updateproductnumber('case',59,true)
 })
 document.getElementById('case-minus').addEventListener('click',function(){
-   updateproductnumber('case',false)
+   updateproductnumber('case',59,false)
 })
