966253c1030964b52919b5a9c1ac86daeb8dbf3c
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-03 23:31:48 +0600

    total shopping done

--- js-shopping-cart/js/phone.js
+++ js-shopping-cart/js/phone.js
@@ -10,6 +10,24 @@ function updateproductnumber(product,price,increaseNumber){
    productInput.value = productNumber;
    const productTotal = document.getElementById(product + '-total');
    productTotal.innerText = productNumber * price;
+   getInputValue()
+}
+
+
+function calcutateTotal(product){
+   const phoductInput = document.getElementById(product + '-number')
+   const productValue = parseInt(phoductInput.value);
+   return productValue;
+}
+function getInputValue(){
+   const phoneTotal = calcutateTotal('phone') * 1219;
+    const caseTotal = calcutateTotal('case') * 59;
+   const subTotal = phoneTotal + caseTotal;
+   const tax = subTotal / 10;
+   const totalAmount = subTotal + tax;
+   document.getElementById('sub-total').innerText=subTotal;
+   document.getElementById('tax-amount').innerText=tax;
+   document.getElementById('total-amount').innerText=totalAmount;
 }
 
 // phone
