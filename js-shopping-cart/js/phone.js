5fb8e8fe91620639da3974b6e15941aee09cbf12
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-03 22:30:42 +0600

    handle the minus button

--- js-shopping-cart/js/phone.js
+++ js-shopping-cart/js/phone.js
@@ -3,3 +3,8 @@ document.getElementById('phone-plus').addEventListener('click',function(){
    const phoneNumber= parseInt(phoneInput.value) + 1;
    phoneInput.value = phoneNumber;
 })
+document.getElementById('phone-minus').addEventListener('click',function(){
+   const phoneInput = document.getElementById('phone-number');
+   const phoneNumber= parseInt(phoneInput.value) - 1;
+   phoneInput.value = phoneNumber;
+})
