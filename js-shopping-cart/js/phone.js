2a8a835ce9ec91d99a2b156c6a555964fb43f04e
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-03 22:43:51 +0600

    handle the total values of phone

--- js-shopping-cart/js/phone.js
+++ js-shopping-cart/js/phone.js
@@ -1,10 +1,22 @@
+function updateproductnumber(increaseNumber){
+   const phoneInput = document.getElementById('phone-number');
+   let phoneNumber= phoneInput.value;
+   if(increaseNumber){
+       phoneNumber= parseInt(phoneInput.value) + 1;
+   }
+   else if(phoneNumber>0){
+       phoneNumber= parseInt(phoneInput.value) - 1;
+   }
+   phoneInput.value = phoneNumber;
+   const phoneTotal = document.getElementById('phone-total');
+   phoneTotal.innerText = phoneNumber * 1219;
+}
+
+// phone
 document.getElementById('phone-plus').addEventListener('click',function(){
-   const phoneInput = document.getElementById('phone-number');
-   const phoneNumber= parseInt(phoneInput.value) + 1;
-   phoneInput.value = phoneNumber;
-})
+   updateproductnumber(true)
+});
 document.getElementById('phone-minus').addEventListener('click',function(){
-   const phoneInput = document.getElementById('phone-number');
-   const phoneNumber= parseInt(phoneInput.value) - 1;
-   phoneInput.value = phoneNumber;
-})
+   updateproductnumber(false)
+});
+
