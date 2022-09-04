3a4ef400ffe0b34e8033d4d4fa39172be471f802
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-04 12:35:25 +0600

    again handle to login form

--- Bank-Tailwind/js/banking js/login.js
+++ Bank-Tailwind/js/banking js/login.js
@@ -1,4 +1,27 @@
 document.getElementById('login-submit').addEventListener('click',function(){
+    const inputField = document.getElementById('user-email');
+    const inputUser = inputField.value;
+
+
+    const passField = document.getElementById('user-password');
+    const passUser = passField.value;
+
+    if((inputUser == 'farhana') && (passUser =='123')){
+        window.location.href='banking.html'
+    }
+    else if((inputUser == 'farhana') && passUser != '123'){
+        passField.value = '';
+        alert('Rong Password')
+    }
+    else{
+        inputField.value = '';
+        passField.value = '';
+        alert('try again')
+    }
+
+})
+
+/* document.getElementById('login-submit').addEventListener('click',function(){
     const inputField = document.getElementById('user-email');const inputValue =inputField.value;
     
 
@@ -12,4 +35,4 @@ document.getElementById('login-submit').addEventListener('click',function(){
     else{
         alert('rong password')
     } 
-})
+}) */
