b6c58288cabacadb9385afefea1acbd19b696abb
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-12 23:42:56 +0600

    connect with minus button

--- js-shopping-cart/js/shopping.js
+++ js-shopping-cart/js/shopping.js
@@ -1,5 +1,16 @@
+function getThePhoneIncrease(){
+    
+}
+
+
+
 document.getElementById('case-plus').addEventListener('click',function(){
     const caseInput = document.getElementById('case-number');
     const caseNumber = parseInt(caseInput.value)+1;
+    caseInput.value =caseNumber;
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = parseInt(caseInput.value)-1;
     caseInput.value =caseNumber;
 })
