9da717ddac7d4390480845efb8e7933ca6cc0fed
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-14 23:51:37 +0600

    type the number

--- pin-matcher/js/main.js
+++ pin-matcher/js/main.js
@@ -1,16 +1,24 @@
 function getPin(){
     const pin = Math.round(Math.random()*10000)
     const pinString = pin + '';
-    if(pinString.length ==4){
+    if(pinString.length == 4){
         return pin;
     }
     else{
-        getPin()
+       return getPin();
     }
 }
 
 
 function generatePin(){
     const pin = getPin();
-    console.log(pin);
+   document.getElementById('display-pin').value = pin;
 }
+
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const keyPad = event.target.innerText;
+    const typeNumber = document.getElementById('type-numbers');const previoustype = typeNumber.value;
+    const newType = previoustype + keyPad;
+    typeNumber.value = newType;
+})
