c47f561a3125b5c2ded7de97073fd7acf3a55925
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-17 00:18:12 +0600

    uptodate pin

--- pin-matcher/js/main.js
+++ pin-matcher/js/main.js
@@ -1,38 +1,4 @@
 function getPin(){
-    const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + '';
-    if(pinString.length==4){
-        return pin;
-    }
-    else{
-        return getPin()
-    }
-}
-
-function generatePin(){
-    const pin = getPin();
-    document.getElementById('display-pin').value = pin;
-}
-document.getElementById('key-pad').addEventListener('click',function(event){
-    const number = event.target.innerText;
-    const displayInput = document.getElementById('type-numbers');
-    if(isNaN(number)){
-        displayInput.value = '';
-    }
-    else{
-        const previoustypeText = displayInput.value;
-        const newNumber = previoustypeText + number;
-        displayInput.value =  newNumber;
-    }
-
-})
-
-function verifyPin(){
-    const pin = document.getElementById('display-pin');
-    const displayPin = document.getElementById('type-numbers');
-}
-
-/* function getPin(){
     const pin = Math.round(Math.random()*10000)
     const pinString = pin + '';
     if(pinString.length == 4){
@@ -77,4 +43,4 @@ function verifyPin(){
         notifyFail.style.display = 'block';
         notifySuccess.style.display = 'none';
     }
-} */
+}
